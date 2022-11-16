de93a88813fb3ccfb4359260db589c4545a66315
Oleh Lytvyn <87479589+Ol9hLytvyn@users.noreply.github.com>
2022-11-16 16:35:31 +0100

    Update arrayTaskTwo.js

--- arrayTaskTwo.js
+++ arrayTaskTwo.js
@@ -1,1 +1,27 @@
+`use strict`;
+/* 
+
+2. **Задача на розворот числа:**
+
+const currentMaxValue = 4589;
+let reverseMaxValue;
+
+// тут ваш код...*
+
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
+
+*/
+
+const currentMaxValue = 4589;
+let reverseMaxValue = currentMaxValue.toString().split('').reverse().join('');
+let toNumber = Number(reverseMaxValue);
+reverseMaxValue = toNumber;
+
+
+
+// = currentMaxValue.toString().split('').reverse().join('') * Math.sign(currentMaxValue);
+
+console.log(reverseMaxValue); //9854
+console.log(typeof reverseMaxValue); //'number'
 
