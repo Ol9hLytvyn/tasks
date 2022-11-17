912a4a8ee37306c82912af09a779f8452bfa6665
Oleh Lytvyn <87479589+Ol9hLytvyn@users.noreply.github.com>
2022-11-17 21:59:42 +0100

    Update arrayTask3.js

--- arrayTask3.js
+++ arrayTask3.js
@@ -1,1 +1,26 @@
+/* 
 
+1. **Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:**
+
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray;
+
+// тут ваш код...*
+
+console.log(productOfArray); // 24
+
+*/
+
+
+const resultsArray = [1, 2, [3, [4]]];
+const flatArray = resultsArray.flat(Infinity);
+let productOfArray = flatArray.reduce((prev, current) => prev * current);
+
+
+
+// let productOfArray = resultsArray.flat(Infinity).map(Number);
+
+
+// start here
+
+console.log(productOfArray); //24
